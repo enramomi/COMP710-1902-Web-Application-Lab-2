b7a1eef102036c07f1fad34006dc2e4d532f7b98
enramomi <m.amargoud@gmail.com>
2019-09-25 17:29:05 +1200

    Logo

---
+++ resources/views/index.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <title>Welcome to Boss Cuts</title>
+    <link rel="stylesheet" type="text/css" href="Style.css">
+
+ <!-- Styles -->
+ <style>
+.{
+    margin: 0px;
+    height: 0px;
+  }
+  
+  body {
+    font-family: Verdana,sans-serif;
+    font-size: 0.9em;
+  }
+  
+  div#Login{
+    text-align: center;
+  
+  }
+  
+  div#tableUser{
+    align: center;
+     padding: 75px;
+  }
+  
+  div#tableRegister{
+    text-align: center;
+  }
+  
+  div#header{
+    padding: 10px;
+    color: white;
+    text-align: left;
+    background-color: darkcyan;
+  }
+  div#footer {
+    position: absolute;
+    padding: 10px;
+    width: 100%;
+    bottom: 0px;
+    color: white;
+    text-align: center;
+    background-color: darkcyan;
+  } 
+</style> 
+  </head>
+  <body>
+  <div id="header">
+      <a href="index">
+            <img src="BOSS-CUTS/public/assets/img/webLogo.png" alt="BossCuts">
+        </a>
+       
+                    <a href="https://laravel.com/docs" style="text-align:right">Home</a>
+                    <a href="https://laracasts.com">About Us</a>
+                    <a href="https://laravel-news.com">Gallery</a>
+                    <a href="https://forge.laravel.com">Services</a>
+                    <a href="https://github.com/laravel/laravel">Contact Us</a>
+            
+    </div>
+    <h1 style="text-align:center">Welcome to Trade Here New Zealand</h1>
+    <p style="text-align:right">
+      <a href="JobDescription.php">Post a job</a>
+      <a href="Index.php?logout='1'">Logout</a>
+    </p>
+    </div> 
+    <div id="footer">
+      <p>© COPYRIGHT BOSSCUTS NEW ZEALAND 2019. ALL RIGHTS RESERVED.</p>
+    </div>
+  </body>
+  </html>
